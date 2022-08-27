595699b9978c8a564580a74948224e04e47fb709
wangchenggong <wangchenggong88@163.com>
2022-08-27 15:29:03 +0800

    并查集的简易版本

---
+++ src/main/java/graph/minimal_spanning_tree/UnionFindSet.java
@@ -0,0 +1,41 @@
+package graph.minimal_spanning_tree;
+
+import graph.Node;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * 并查集简易版本(用于最小生成树的K算法)
+ * @author admin
+ * @date 2022/8/27 15:21
+ */
+public class UnionFindSet {
+
+    public HashMap<Node, List<Node>> setMap;
+
+    public UnionFindSet(List<Node> nodes){
+        for(Node node : nodes){
+            List<Node> list = new ArrayList<>();
+            list.add(node);
+            setMap.put(node, list);
+        }
+     }
+
+
+     public boolean isSameSet(Node node1, Node node2){
+        return setMap.get(node1) == setMap.get(node2);
+     }
+
+     public void union(Node node1, Node node2){
+         List<Node> list1 = setMap.get(node1);
+         List<Node> list2 = setMap.get(node2);
+
+         for(Node node : list2){
+             list1.add(node);
+             setMap.put(node, list1);
+         }
+     }
+
+}
