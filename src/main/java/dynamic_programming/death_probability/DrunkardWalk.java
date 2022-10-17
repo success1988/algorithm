b5bf6a6e68950c7a089da7feeb66c2a5f95be177
wangchenggong <wangchenggong88@163.com>
2022-10-18 07:58:00 +0800

    死亡概率的计算

--- src/main/java/dynamic_programming/death_probability/DrunkardWalk.java
+++ src/main/java/dynamic_programming/death_probability/DrunkardWalk.java
@@ -1,8 +1,47 @@
 package dynamic_programming.death_probability;
 
 /**
+ * 在N行M列的坐标区域内，右上角坐标为(N-1,M-1)
+ * 当前醉汉的坐标为(row,col)
+ * 醉汉等概率地上下左右移动，每次移动一步，超出坐标区域时直接死亡
+ * 求k步之后，醉汉还存活的概率
  * @author admin
  * @date 2022/10/7 20:46
  */
 public class DrunkardWalk {
+
+
+    public static void main(String[] args) {
+        int N = 10;
+        int M = 9;
+        int row = 6;
+        int col = 5;
+        int k = 7;
+
+        double aliveProbability = getAliveProbability(N, M, row, col, k);
+        System.out.println("[暴力递归]醉汉还存活的概率为:"+aliveProbability);
+
+    }
+
+    private static double getAliveProbability(int n, int m, int row, int col, int k) {
+        int aliveWays = process(n, m, row, col, k);
+        return (double)aliveWays/Math.pow(4, k);
+    }
+
+    private static int process(int n, int m, int row, int col, int rest) {
+        if(row < 0 || row == n || col < 0 || col == m){
+            return 0;
+        }
+        if(rest == 0){
+            return 1;
+        }
+        int ways = 0;
+        ways += process(n, m, row+1, col, rest-1);
+        ways += process(n, m, row-1, col, rest-1);
+        ways += process(n, m, row, col+1, rest-1);
+        ways += process(n, m, row, col-1, rest-1);
+        return ways;
+    }
+
+
 }
--- src/main/java/dynamic_programming/death_probability/HeroKillMonster.java
+++ src/main/java/dynamic_programming/death_probability/HeroKillMonster.java
@@ -1,8 +1,47 @@
 package dynamic_programming.death_probability;
 
 /**
+ * 给定3个参数: N,M,K
+ * 怪兽有N滴血, 等着英雄来砍自己
+ * 英雄每一次打击，都会让怪兽流失[0~M]的血量
+ * 到底流失多少呢？每一次在[0~M]上等概率的获取一个值
+ * 求k次打击之后，英雄把怪兽砍死的概率
  * @author admin
  * @date 2022/10/7 20:51
  */
 public class HeroKillMonster {
+
+
+    public static void main(String[] args) {
+        int N = 10;
+        int M = 3;
+        int k = 5;
+
+        double deathProbability = getDeathProbability(N, M, k);
+        System.out.println("[暴力递归]英雄砍死怪兽的概率为:"+deathProbability);
+
+    }
+
+    private static double getDeathProbability(int n, int m, int k) {
+        // m+1种可能性
+        int totalWays = (int)Math.pow(m+1, k);
+        int aliveWays = process(n, m, k);
+        return (double)(totalWays-aliveWays)/(double)totalWays;
+    }
+
+    private static int process(int restBlood, int m, int restHit) {
+        if(restBlood <= 0){
+            return 0;
+        }
+        if(restHit == 0){
+            return 1;
+        }
+
+        int ways = 0;
+        for (int i = 0; i <= m; i++) {
+            ways += process(restBlood-i, m, restHit-1);
+        }
+        return ways;
+    }
+
 }
